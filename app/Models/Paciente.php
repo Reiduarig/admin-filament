ee6459afca404802755a1b14704a370fdcb60d6c
Reiduarig <giraudier82@gmail.com>
2023-08-12 21:29:48 +0200

    cast precio modelo tratamiento

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Paciente extends Model
 {
-    public function pacientes(): HasMany
+    public function cliente(): BelongsTo
     {
         return $this->belongsTo(Cliente::class);
     }
--- app/Models/Tratamiento.php
+++ app/Models/Tratamiento.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use App\Casts\PrecioCast;
 
 class Tratamiento extends Model
 {
+    protected $casts = [
+        'preciop' => PrecioCast::class,
+    ];
+
     public function paciente(): HasMany
     {
         return $this->belongsTo(Paciente::class);
